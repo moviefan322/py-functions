5e181fc7c81efc5776e3e3bf2c3f75a742cb3a43
moviefan322 <philman202@yahoo.com>
2023-07-07 17:37:00 -0400

    writes fizz buzz game

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -17,6 +17,18 @@ def fizz_buzz(num: int) -> str:
         return str(num)
 
 
-for i in range(100):
-    result = fizz_buzz(i)
-    print(result)
+input("Play Fizz Buzz.   Press ENTER to start")
+print()
+
+next_number = 0
+while next_number < 99:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    players_answer = input("Your go: ")
+    if players_answer != correct_answer:
+        print("You lose, the correct answer was {}".format(correct_answer))
+        break
+else:
+    print("Well done, you reached {}".format(next_number))
