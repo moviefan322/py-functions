e893dbc574ec36dff280345d84f806d52c057e1b
moviefan322 <philman202@yahoo.com>
2023-07-07 00:48:50 -0400

    adds palindrome function

--- functions.py
+++ functions.py
@@ -1,16 +1,33 @@
+abc = "abcdefghijklmnopqrstuvwxyz"
 def multiply(x, y):
     result = x * y
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
+def palindrome_sentence(string):
+    folded = string.casefold()
+    sanitized = []
+    for char in folded:
+        if char in abc:
+            sanitized.append(char)
+    joined = "".join(sanitized)
+    return joined[::-1] == joined
 
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+
+# word = input("Please enter a word to check: ")
+# if is_palindrome(word):
+#     print("'{}' is a palindrome".format(word))
+# else:
+#     print("'{}' is not a palindrome".format(word))
+
+word = input("Please enter a sentence to check: ")
+if palindrome_sentence(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
