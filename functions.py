9b7a03533d1f02b4496dafad23d7620524ba2405
moviefan322 <philman202@yahoo.com>
2023-07-07 11:41:23 -0400

    writes fibonacci function

--- functions.py
+++ functions.py
@@ -64,5 +64,21 @@ def palindrome2(sentence):
 # else:
 #     print("'{}' is not a palindrome".format(word))
 
-answer = multiply(18, 3)
-print(answer)
+def fibonacci(n):
+    """Return the `n` th Fibonacci number, for positive `n`"""
+    if 0 <= n <= 1:
+        return n
+
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
+for i in range(36000):
+    print(i, fibonacci(i))
